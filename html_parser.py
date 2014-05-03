23383368b7cc58307d20cc488bc93ee77fcf7714
Joe <Joe@localhost>
2014-05-03 11:27:53 +0100

    add: get date of quotes

--- html_parser.py
+++ html_parser.py
@@ -5,10 +5,7 @@ Created on Thu May 01 22:43:26 2014
 @author: Shengzhou
 """
 
-import time
-import urllib
-import sys
-import string
+import time, urllib, sys, string
 from HTMLParser import HTMLParser
 
 ticker_list = ["socl", "ibb", "pnqi", "eirl", "ita", "iai", "xsd", "vbk", "dfe", "qqq", "ewi", "pbd"]
@@ -17,24 +14,43 @@ ticker = ""
 class MyHTMLParser(HTMLParser):
     previous_data = ""
     text_before_amp = ""
-    
+
+    def handle_starttag(self, tag, attrs):
+        # get arrow diection: up/positive or down/negative
+        for attr in attrs:
+            if "down_r time_rtq_content"==attr[1]:
+                sys.stdout.write("\t-")
+            if "up_g time_rtq_content"==attr[1]:
+                sys.stdout.write("\t+")
+
     def handle_data(self, data):
         starttag_text = self.get_starttag_text()
-        
+
+        # get stock/fund name
         if -1!=string.find(data, ("(%s)" % ticker).upper()) and -1!=string.find(starttag_text, "<h2>"):
-            sys.stdout.write(time.strftime("%d/%m/%Y") + "\t")
             if ""!=self.text_before_amp:
                 sys.stdout.write(self.text_before_amp + "&")
             sys.stdout.write(data)
-            
+
+        # get percentage changed
+        if -1!=string.find(str(starttag_text), "yfs_p43_%s" % ticker.lower()) and -1!=string.find(data, "("):
+            sys.stdout.write(data.replace("(", "").replace(")", ""))
+
+        # get date of quotes
+        if -1!=string.find(str(starttag_text), "yfs_t53_%s" % ticker.lower()):
+            t = time.strptime(data.split(",")[0] + time.strftime(" %Y"), "%b %d %Y")
+            sys.stdout.write("\t" + time.strftime("%d/%m/%Y", t))
+
+        # get the day's range - lower value
         if -1!=string.find(str(starttag_text), "yfs_g53_%s" % ticker.lower()) and -1==string.find(data, "-"):
             sys.stdout.write("\t" + data)
-            
+
+        # get the day's range - upper value
         if -1!=string.find(str(starttag_text), "yfs_h53_%s" % ticker.lower()):
             print "\t", data
-            
+
         self.previous_data = data
-            
+
     def handle_entityref(self, name):
         if "amp"==name and -1!=string.find(self.get_starttag_text(), "<h2>"):
             self.text_before_amp = self.previous_data
--- readme.txt
+++ readme.txt
@@ -1,15 +1,18 @@
-This is a HTML parser written in Python. It fetches the name and Day's Range of securities from Yahoo Finance.
+This is a HTML parser written in Python 2.7.6. It fetches the name and quotes of securities from Yahoo Finance.
 
 Sample output:
-03/05/2014	Global X Social Media Index ETF (SOCL)	17.41	17.83
-03/05/2014	iShares Nasdaq Biotechnology (IBB)	227.70	233.07
-03/05/2014	PowerShares NASDAQ Internet (PNQI)	62.15	62.98
-03/05/2014	iShares MSCI Ireland Capped (EIRL)	38.28	38.79
-03/05/2014	iShares US Aerospace & Defense (ITA)	108.32	109.70
-03/05/2014	iShares US Broker-Dealers (IAI)	37.46	37.89
-03/05/2014	SPDR S&P Semiconductor ETF (XSD)	66.89	67.59
-03/05/2014	Vanguard Small Cap Growth ETF (VBK)	119.46	120.70
-03/05/2014	WisdomTree Europe SmallCap Dividend (DFE)	62.02	62.61
-03/05/2014	PowerShares QQQ (QQQ)	87.28	88.11
-03/05/2014	iShares MSCI Italy Capped (EWI)	17.89	18.03
-03/05/2014	PowerShares Global Clean Energy (PBD)	13.02	13.08
+Global X Social Media Index ETF (SOCL)	-0.91%	02/05/2014	17.41	17.83
+iShares Nasdaq Biotechnology (IBB)	-1.48%	02/05/2014	227.70	233.07
+PowerShares NASDAQ Internet (PNQI)	-0.37%	02/05/2014	62.15	62.98
+iShares MSCI Ireland Capped (EIRL)	+0.04%	02/05/2014	38.28	38.79
+iShares US Aerospace & Defense (ITA)	+0.68%	02/05/2014	108.32	109.70
+iShares US Broker-Dealers (IAI)	+0.13%	02/05/2014	37.46	37.89
+SPDR S&P Semiconductor ETF (XSD)	+0.10%	02/05/2014	66.89	67.59
+Vanguard Small Cap Growth ETF (VBK)	+0.32%	02/05/2014	119.46	120.70
+WisdomTree Europe SmallCap Dividend (DFE)	+0.19%	02/05/2014	62.02	62.61
+PowerShares QQQ (QQQ)	-0.18%	02/05/2014	87.28	88.11
+iShares MSCI Italy Capped (EWI)	-0.33%	02/05/2014	17.89	18.03
+PowerShares Global Clean Energy (PBD)	+0.31%	02/05/2014	13.02	13.08
+
+For more information on HTMLParser, see
+https://docs.python.org/2/library/htmlparser.html
