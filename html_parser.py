3cec226f288e0001d2de0604fa6b319790ecf7e0
Joe <Joe@localhost>
2014-05-05 11:29:19 +0100

    update output format

--- html_parser.py
+++ html_parser.py
@@ -14,46 +14,55 @@ ticker = ""
 class MyHTMLParser(HTMLParser):
     previous_data = ""
     text_before_amp = ""
+    negative = False
 
     def handle_starttag(self, tag, attrs):
         # get arrow diection: up/positive or down/negative
         for attr in attrs:
-            if "down_r time_rtq_content"==attr[1]:
-                sys.stdout.write("\t-")
             if "up_g time_rtq_content"==attr[1]:
-                sys.stdout.write("\t+")
+                self.negative = False
+            if "down_r time_rtq_content"==attr[1]:
+                self.negative = True
 
     def handle_data(self, data):
         starttag_text = self.get_starttag_text()
 
         # get stock/fund name
-        if -1!=string.find(data, ("(%s)" % ticker).upper()) and -1!=string.find(starttag_text, "<h2>"):
+        if -1 != string.find(data, ("(%s)" % ticker).upper()) and -1 != string.find(starttag_text, "<h2>"):
+            sys.stdout.write(ticker + "\t")
             if ""!=self.text_before_amp:
                 sys.stdout.write(self.text_before_amp + "&")
             sys.stdout.write(data)
-
+            
+        # get quote
+        if -1 != string.find(str(starttag_text), "yfs_l84_%s" % ticker.lower()) and len(data.strip()) > 0:
+            sys.stdout.write("\t" + data)
+            
         # get percentage changed
-        if -1!=string.find(str(starttag_text), "yfs_p43_%s" % ticker.lower()) and -1!=string.find(data, "("):
-            sys.stdout.write(data.replace("(", "").replace(")", ""))
+        if -1 != string.find(str(starttag_text), "yfs_p20_%s" % ticker.lower()) and len(data.strip()) > 0:
+            sys.stdout.write("\t")
+            if self.negative:
+                sys.stdout.write("-")
+            sys.stdout.write(data.strip("()"))
 
-        # get date of quotes
-        if -1!=string.find(str(starttag_text), "yfs_t53_%s" % ticker.lower()):
+        # get date of quote
+        if -1 != string.find(str(starttag_text), "yfs_t53_%s" % ticker.lower()):
             t = time.strptime(data.split(",")[0] + time.strftime(" %Y"), "%b %d %Y")
             sys.stdout.write("\t" + time.strftime("%d/%m/%Y", t))
 
         # get the day's range - lower value
-        if -1!=string.find(str(starttag_text), "yfs_g53_%s" % ticker.lower()) and -1==string.find(data, "-"):
+        if -1 != string.find(str(starttag_text), "yfs_g53_%s" % ticker.lower()) and len(data.strip(" -")) > 0:
             sys.stdout.write("\t" + data)
 
         # get the day's range - upper value
-        if -1!=string.find(str(starttag_text), "yfs_h53_%s" % ticker.lower()):
-            print "\t", data
+        if -1 != string.find(str(starttag_text), "yfs_h53_%s" % ticker.lower()):
+            print "\t" + data
 
         self.previous_data = data
 
     def handle_entityref(self, name):
         # for stock names with the symbol &, save the part before & for later use
-        if "amp"==name and -1!=string.find(self.get_starttag_text(), "<h2>"):
+        if "amp" == name and -1 != string.find(self.get_starttag_text(), "<h2>"):
             self.text_before_amp = self.previous_data
 
 for t in ticker_list:
