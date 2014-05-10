01220e735253f199e7b330eb80861341fae5280b
Joe <Joe@localhost>
2014-05-10 21:33:13 +0100

    update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python parser.py "exi doo rpv idv jki ewg dfe ezu voe schc ewq vis xli fez eirl ixj sphb vgt ewp ita ewi qqq fpx rpg vbk vht rzv pnqi socl ibb" > output.txt
+python parser.py "exi doo rpv ewg dfe ezu schc ewq ezm vis xli fez eirl ryt ixj sphb vgt ewp ita ewi qqq fpx rpg vbk vht rzv psct pnqi socl ibb" > output.txt
 ren output.txt quotes_%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt
 
 REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
