c97479f6ce6c52253543668768a8d29182ff038e
Joe <Joe@localhost>
2014-05-10 17:27:54 +0100

    update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,5 @@
-python parser.py "scz exi iefa doo rpv iev vgk idv jki ewg dfe ezu voe spy schc ewq vis xli fez eirl ixj vgt ewp ita ewi qqq vbk pnqi socl ibb" > output.txt
-ren output.txt quotes_%DATE:/=%_%time:~0,2%%time:~3,2%.txt
+python parser.py "scz exi iefa doo rpv iev vgk idv jki ewg dfe ezu voe spy schc ewq vis piz xli fez eirl ixj sphb vgt ewp ita ewi qqq vbk pnqi socl ibb" > output.txt
+ren output.txt quotes_%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt
+
+REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
