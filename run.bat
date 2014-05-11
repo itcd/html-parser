231968f11253e173e9a339a5a21bcd793c0cc969
Joe <Joe@localhost>
2014-05-11 15:58:18 +0100

    update ETF list

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python parser.py "rpv ewg dfe ezu ewq ezm vis eirl ryt sphb ewp ita ewi qqq fpx rpg vbk pnqi socl ibb" > output.txt
+python parser.py "rpv ewg dfe ezu ewq vis eirl ryt sphb ewp ita ewi qqq fpx rpg vbk pnqi xph socl ibb" > output.txt
 ren output.txt quotes_%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt
 
 REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
