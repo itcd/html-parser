2f6f1dee7e02bc1c7b7bb54f7097f8ea2ea1290c
Joe <Joe@localhost>
2014-05-15 00:11:33 +0100

    update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python parser.py "ezu dfe ewp gulf ewg ewi sphb rpv fpx qqq pbd rpg vis fm cqqq xph pnqi eirl ita socl" > quotes_%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt
+python parser.py "ezu dfe ewp gulf ewg ewi sphb rpv fpx qqq pbd rpg vis fm cqqq xph pnqi eirl ita socl" > "quotes_%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt"
 
 REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
