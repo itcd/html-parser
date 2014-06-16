219ccfb09814d64dad3845a78166e7c5aa54430a
Joe <Joe@localhost>
2014-06-16 22:07:12 +0100

    update ticker list

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python parser.py "ezu rpv dfe ita ewp ewi fpx qqq rpg sphb pbd pnqi xph socl pgj cqqq eden fm eirl gulf" > "~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt"
+python parser.py "feeu rpv dfe ita ewp ewi fpx qqq rpg sphb pbd pnqi xph socl pgj cqqq eden fm eirl gulf" > "~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt"
 
 REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
