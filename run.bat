5536b4ce7d0aca033c6738b2d853ffd4d1ad8de2
Joe <Joe@localhost>
2014-07-04 00:18:54 +0100

    update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-python parser.py "eirl dfe ita vbk rpv fpx figy rpg feeu xph eden sphb pgj ibb cqqq pbd ewp xsd pnqi socl" > "~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt"
+python parser.py "vbk fpx ewp ewi pnqi ita eden pgj pbd dfe figy xph cqqq socl xsd ibb feeu eirl" > "~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%.txt"
 
 REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
