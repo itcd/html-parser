36ea4cd24a9152fd790f466160cde9194fdb8ef9
Joe <Joe@localhost>
2014-07-15 17:38:46 +0100

    run.bat and schedule.bat are working now

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
 @ECHO ON
-SET filename="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%.txt"
-IF DEFINED target_dir ( SET filename="%target_dir:"=%%filename:"=%" ) ELSE (ECHO target_dir is not defined.)
-IF NOT DEFINED target_dir ( SET filename="%~dp0%filename:"=%" ) ELSE (ECHO target_dir is defined.)
+SET datetime="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%.txt"
+IF DEFINED target_dir ( SET filename="%target_dir:"=%%datetime:"=%" )
+IF NOT DEFINED target_dir ( SET filename="%~dp0%datetime:"=%" )
 python "%~dp0parser.py" "vbk ewp ewi pnqi ita eden pgj pbd dfe figy xph cqqq socl ibb feeu eirl" > %filename%
--- schedule.bat
+++ schedule.bat
@@ -1,4 +1,6 @@
-Echo Launch dir: "%~dp0"
-Echo Current dir: "%CD%"
+@ECHO OFF
+REM Echo Launch dir: "%~dp0"
+REM Echo Current dir: "%CD%"
+@ECHO ON
 SET target_dir="D:/Dropbox/quotes/"
 call "%~dp0run.bat"
