362d6fc94047649963188b1db6aacf73a0571bda
Joe <Joe@localhost>
2014-07-15 16:51:13 +0100

    update run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@
 REM YYYYmmdd_HHMMSS %date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
 @ECHO ON
-SET file="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%.txt"
-IF DEFINED target_dir (SET filename="%target_dir:"=%%file:"=%") ELSE (SET filename=%file%)
+SET filename="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%.txt"
+IF DEFINED target_dir (SET filename="%target_dir:"=%%filename:"=%")
 python parser.py "vbk ewp ewi pnqi ita eden pgj pbd dfe figy xph cqqq socl ibb feeu eirl" > %filename%
