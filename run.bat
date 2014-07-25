0d7deb6eb58164c01e6a8a283ce1a4d945152845
Joe <Joe@localhost>
2014-07-26 00:10:54 +0100

    edit run.bat again

--- run.bat
+++ run.bat
@@ -5,4 +5,4 @@ REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
 SET datetime="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%.txt"
 IF DEFINED target_dir ( SET filename="%target_dir:"=%%datetime:"=%" )
 IF NOT DEFINED target_dir ( SET filename="%~dp0%datetime:"=%" )
-python "%~dp0parser.py" "dfe eden iyw eirl ewp ieo vbk ewi xph pnqi figy socl cqqq xtn feeu pgj" > %filename%
+python "%~dp0parser.py" "dfe eden iyw eirl ewp ieo vbk ewi xph pnqi figy cqqq xtn feeu pgj grek" > %filename%
