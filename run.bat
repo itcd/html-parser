2cd55ac0c6a5106b9015d003db3d4643008daedf
Joe <Joe@localhost>
2014-07-25 17:24:43 +0100

    replace fbg with sphb

--- run.bat
+++ run.bat
@@ -5,4 +5,4 @@ REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
 SET datetime="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%.txt"
 IF DEFINED target_dir ( SET filename="%target_dir:"=%%datetime:"=%" )
 IF NOT DEFINED target_dir ( SET filename="%~dp0%datetime:"=%" )
-python "%~dp0parser.py" "fbg figy dfe vbk cqqq xph feeu eirl soxx eden pgj pnqi tan ibb ewi ewp" > %filename%
+python "%~dp0parser.py" "sphb figy dfe vbk cqqq xph feeu eirl soxx eden pgj pnqi tan ibb ewi ewp" > %filename%
