dfbbe14a9306e62ef2d2ff5ad19d3292bac76d16
Joe <Joe@localhost>
2014-08-14 22:19:12 +0100

    update 5 tickers

--- run.bat
+++ run.bat
@@ -5,4 +5,4 @@ REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
 SET time_str="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%.txt"
 IF DEFINED target_dir ( SET filename="%target_dir:"=%%time_str:"=%" )
 IF NOT DEFINED target_dir ( SET filename="%~dp0%time_str:"=%" )
-python "%~dp0parser.py" "dfe eden iyw eirl ewp vbk ewi xph pnqi figy cqqq xtn feeu pgj thd kweb" > %filename%
+python "%~dp0parser.py" "eden iyw ewp thd figy pnqi xtn dfe mlpx feeu pgj pbe socl cqqq eirl kweb" > %filename%
