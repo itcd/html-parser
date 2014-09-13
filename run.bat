07ce0fecb6711e6363d6aded3325f6213f746644
Joe <Joe@localhost>
2014-09-13 23:29:11 +0100

    change a ticker

--- run.bat
+++ run.bat
@@ -5,4 +5,4 @@ REM ddmmYYYY_HHMM %date:/=%_%time:~0,2%%time:~3,2%
 SET time_str="~%date:~-4%%date:~3,2%%date:~0,2%_%time:~0,2%%time:~3,2%_%time:~6,2%.txt"
 IF DEFINED target_dir ( SET filename="%target_dir:"=%%time_str:"=%" )
 IF NOT DEFINED target_dir ( SET filename="%~dp0%time_str:"=%" )
-python "%~dp0parser.py" "itlt dfe pnqi cqqq pgj socl ewp kweb xph blv vbk eden soxx zroz mlpx xiv" > %filename%
+python "%~dp0parser.py" "dfe pnqi cqqq pgj socl ewp kweb trnd xph blv vbk eden soxx zroz mlpx xiv" > %filename%
